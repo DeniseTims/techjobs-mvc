8f9dd09d9f96e64292cddd7545bc432cbd0f074c
Denise Tims <denise.tims@gmail.com>
2018-06-05 19:42:37 -0500

    techjobs mvc, done, denise tims

--- src/main/java/org/launchcode/TechjobsApplication.java
+++ src/main/java/org/launchcode/TechjobsApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TechjobsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(TechjobsApplication.class, args);
 	}
--- src/main/java/org/launchcode/controllers/HomeController.java
+++ src/main/java/org/launchcode/controllers/HomeController.java
@@ -14,14 +14,10 @@ public class HomeController {
 
     @RequestMapping(value = "")
     public String index(Model model) {
-
         HashMap<String, String> actionChoices = new HashMap<>();
         actionChoices.put("search", "Search");
         actionChoices.put("list", "List");
-
         model.addAttribute("actions", actionChoices);
-
         return "index";
     }
-
 }
